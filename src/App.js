f19b5ff112028935da325becc7269eef3ad76151
josex2r <jlrepresa@gmail.com>
2020-05-23 10:27:39 +0200

    feat(app): color picker

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { SliderPicker } from 'react-color';
 
 function App() {
+  const [color, setColor] = useState('#fff');
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Bluetooth <code>Lightbulb</code></p>
+
+        <div className="container">
+          <SliderPicker
+            color={ color }
+            onChangeComplete={ setColor }
+          />
+        </div>
       </header>
     </div>
   );
